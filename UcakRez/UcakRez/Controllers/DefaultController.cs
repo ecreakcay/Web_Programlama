b4d0f43e21d6a3e26d9e9d90fbb57b55a6567d04
ecreakcay <ecreakcay@gmail.com>
2023-12-02 22:20:27 +0300

    Proje giriş işlemleri

---
+++ UcakRez/UcakRez/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UcakRez.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
