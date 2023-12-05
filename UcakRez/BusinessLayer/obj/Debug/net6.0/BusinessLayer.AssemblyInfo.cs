744f6c4221fae65e3bb9ddd08bec312be7ee9ddf
ecreakcay <ecreakcay@gmail.com>
2023-12-05 03:49:10 +0300

    Randevu İşlemleri

--- UcakRez/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ UcakRez/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b290537c8cd6b00c8f08310a04bba909380e320")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
